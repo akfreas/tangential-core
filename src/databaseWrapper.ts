3c193fb026c808d051cba30204e01e73b09a3118
Alexander Freas <akfreas@gmail.com>
2023-11-05 09:00:22 +0100

    Add db wrapper, move exports around

---
+++ src/databaseWrapper.ts
@@ -0,0 +1,75 @@
+import {
+  MongoClient, Db, Collection,
+  OptionalUnlessRequiredId, InsertOneResult,
+  ServerApiVersion, Document
+} from 'mongodb';
+import { doLog, jsonLog } from './logging';
+
+export class MongoDBWrapper {
+  private static instance: MongoDBWrapper;
+  private client?: MongoClient;
+  public db?: Db;
+
+  private databaseUrl: string;
+  private databaseName: string;
+
+  private constructor(url: string, database: string) {
+    this.databaseUrl = url;
+    this.databaseName = database;
+  }
+
+  public static async getInstance(url?: string, database?: string): Promise<MongoDBWrapper> {
+
+    if (!url || !database) {
+      throw new Error('MongoDBWrapper.getInstance() requires url and database parameters');
+    }
+
+
+    if (!this.instance) {
+      this.instance = new MongoDBWrapper(url, database);
+      await this.instance.connect();
+    }
+    return this.instance;
+  }
+
+  private async connect(): Promise<void> {
+    try {
+
+      let options = {
+        serverApi: {
+          version: ServerApiVersion.v1,
+          strict: true,
+          deprecationErrors: true,
+        }
+      }
+      // console.log('Connecting to MongoDB...', url);
+      // if (process.env.LOCAL_HTTP_PROXY) {
+      //   options = {
+      //     ...options,
+      //     ...{
+      //       proxyHost: process.env.LOCAL_HTTP_PROXY.split(':')[0],
+      //       proxyPort: parseInt(process.env.LOCAL_HTTP_PROXY.split(':')[2])
+      //     },
+      //   }
+      // }
+      this.client = new MongoClient(this.databaseUrl, options);
+      this.db = this.client.db(this.databaseName); // You can specify a database name here if needed
+    } catch (error) {
+      console.error('Failed to connect to MongoDB:', error);
+    }
+  }
+
+  public getCollection<T extends Document>(name: string): Collection<T> {
+    if (!this.db) {
+      throw new Error('Database connection not established');
+    }
+    return this.db.collection<T>(name);
+  }
+
+  public async storeDocument<T extends Document>(collection: Collection<T>, document: OptionalUnlessRequiredId<T>): Promise<void> {
+    const result: InsertOneResult<T> = await collection.insertOne(document);
+    if (result.acknowledged !== true) {
+      throw new Error(`Failed to insert document into collection ${collection.collectionName}`);
+    }
+  }
+}
---
+++ src/jiraTypes.ts
@@ -0,0 +1,134 @@
+import { Document } from "mongodb";
+
+export interface ChangelogEntry {
+  issue_id: string;
+  key: string;
+  changelog: any[];
+}
+
+export interface JiraRequestAuth {
+  accessToken: string;
+  atlassianId: string;
+}
+
+export interface JiraRequestOptions {
+  path: string;
+  method: string;
+  body?: any;
+  params?: any;
+}
+
+export interface GetByJqlResponse {
+  issues: any[];
+}
+
+export interface PointsField {
+  id: string;
+}
+export interface AvatarUrls {
+  "48x48": string;
+  "24x24": string;
+  "16x16": string;
+  "32x32": string;
+}
+
+export interface Author {
+  accountId: string;
+  displayName: string;
+}
+
+export interface ChangelogItem {
+  field: string;
+  fieldtype: string;
+  fieldId: string; // New field
+  from: string | null;
+  fromString: string;
+  to: string | null;
+  toString: string;
+}
+
+export interface ChangelogValue {
+  id: string;
+  author: Author;
+  created: string;
+  items: ChangelogItem[];
+}
+
+export interface Changelog {
+  self: string; // New field
+  nextPage: string; // New field
+  maxResults: number; // New field
+  startAt: number; // New field
+  total: number; // New field
+  isLast: boolean; // New field
+  values: ChangelogValue[]; // Updated field type
+}
+
+
+export interface ChildIssue {  // probably rename to "ticket" or "story"
+  id: string;
+  key: string;
+  fields?: any;
+}
+
+export interface LongRunningIssue {
+  id: string;
+  key: string;
+  self: string;
+  timeInStatus: number;
+}
+
+export interface EpicReport {
+  epicKey: string;
+  velocity: number;
+  changelogs: ChangelogEntry[];
+  status: string;
+  longRunningIssues: LongRunningIssue[];
+  childIssues: ChildIssue[];
+  summary: string;
+  generatedSummary: string;
+}
+
+export interface JiraProfile {
+  accountId: string;
+  avatarUrls: AvatarUrls;
+  displayName: string;
+}
+
+export type IssueComment = {
+  self: string;
+  id: string;
+  author: JiraProfile;
+  body: {
+    type: string;
+    version: number;
+    content: Array<{
+      type: string;
+      content: Array<{
+        type: string;
+        text: string;
+      }>;
+    }>;
+  };
+  updateAuthor: JiraProfile;
+  created: string;
+  updated: string;
+  visibility: {
+    type: string;
+    value: string;
+    identifier: string;
+  };
+};
+
+
+export interface ProjectReport extends Document {
+  velocity: number;
+  name: string;
+  lead: JiraProfile;
+  active: boolean;
+  epics: EpicReport[];
+  windowStartDate: string;
+  windowEndDate: string;
+  projectKey: string;
+}
+
